6793233626de95d49b3a70326f83d4a8c545ccba
Paul Bird <paul.bird@hawkeyeinnovations.com>
2023-04-08 16:00:34 +0100

    feat(reputation-manager): reputation manager for dealing with prestige/notoriety between factions

--- src/components/reputationManager.tsx
+++ src/components/reputationManager.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { cn } from '@/lib/utils'
+
 const notorietyStepCount = 3
 const prestigeStepCount = 5
 
@@ -9,53 +11,143 @@ const notorietyLevels = [3, 2, 1]
 const prestigeSteps = new Array(prestigeStepCount).fill(0)
 const prestigeLevels = [1, 2, 3]
 
-interface ReputationState {
-  level: number
-  prestigeCount: number
-  notorietyCount: number
-}
-
 interface ReputationManagerRowProps {
   faction: FactionReputation
+  onChange?: (faction: FactionReputation) => void
 }
 
 interface FactionReputation {
   id: string
   name: string
   level: number
+  prestigeLevel: number
   prestigeCount: number
+  notorietyLevel: number
+  notorietyCount: number
+}
+
+interface FactionCountAndLevels {
+  prestigeLevel: number
+  prestigeCount: number
+  notorietyLevel: number
   notorietyCount: number
 }
 
 // const factions = ['Marquisate', 'Eyrie Dynasty', 'Woodland Alliance', 'Lizard Cult', 'Riverfolk Company', 'Denizens']
 
-const factions: FactionReputation[] = [
+const factionsState: FactionReputation[] = [
   {
     id: 'marquisate',
     name: 'Marquisate',
     level: 0,
+    prestigeLevel: 0,
     prestigeCount: 0,
+    notorietyLevel: 0,
     notorietyCount: 0,
   },
   {
     id: 'eyrie-dynasty',
     name: 'Eyrie Dynasty',
     level: 0,
+    prestigeLevel: 0,
     prestigeCount: 0,
+    notorietyLevel: 0,
     notorietyCount: 0,
   },
   {
     id: 'woodland-alliance',
     name: 'Woodland Alliance',
     level: 0,
+    prestigeLevel: 0,
     prestigeCount: 0,
+    notorietyLevel: 0,
     notorietyCount: 0,
   },
 ]
 
-const ReputationManagerRow = ({ faction }: ReputationManagerRowProps) => {
+// next emit an event from the checkbox noting it's index nad faction to lift state back up
+
+const ReputationManagerRow = ({ faction, onChange }: ReputationManagerRowProps) => {
+  // const notorietyThreshold = faction.level < 0 ? (faction.level * -1 + 1) * notorietyStepCount : notorietyStepCount
+  // const prestigeThreshold = faction.level > 0 ? (faction.level + 1) * prestigeStepCount : prestigeStepCount
+  const notorietyThreshold =
+    faction.notorietyLevel < 0 ? (faction.notorietyLevel * -1 + 1) * notorietyStepCount : notorietyStepCount
+  const prestigeThreshold =
+    faction.prestigeLevel > 0 ? (faction.prestigeLevel + 1) * prestigeStepCount : prestigeStepCount
+
+  // make this a recursive function that will keep calculating until correct levels and thresholds are balanced
+  const calculateReputationLevels = (
+    notorietyLevel: number,
+    prestigeLevel: number,
+    notorietyCount: number,
+    prestigeCount: number
+  ): FactionCountAndLevels => {
+    const notorietyThreshold = notorietyLevel < 0 ? (notorietyLevel * -1 + 1) * notorietyStepCount : notorietyStepCount
+    const prestigeThreshold = prestigeLevel > 0 ? (prestigeLevel + 1) * prestigeStepCount : prestigeStepCount
+
+    if (prestigeCount >= prestigeThreshold || notorietyCount >= notorietyThreshold) {
+      // handles left over values from our count into the threshold
+      const newNotoriety = notorietyCount % notorietyThreshold
+      const newPrestige = prestigeCount % prestigeThreshold
+      const notorietyLevel = Math.floor(notorietyCount / notorietyStepCount) * -1
+      const prestigeLevel = Math.floor(prestigeCount / prestigeStepCount)
+
+      return calculateReputationLevels(notorietyLevel, prestigeLevel, newNotoriety, newPrestige)
+    } else {
+      return {
+        notorietyLevel,
+        prestigeLevel,
+        notorietyCount,
+        prestigeCount,
+      }
+    }
+  }
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value)
+    const name = e.target.name
+    const checked = e.target.checked
+
+    let newValue = value
+
+    if (checked) {
+      newValue = value
+    }
+    if (!checked && value !== 0) {
+      newValue = value - 1
+    }
+
+    const prestigeCount = name === 'prestigeCount' ? newValue : faction.prestigeCount
+    const notorietyCount = name === 'notorietyCount' ? newValue : faction.notorietyCount
+
+    // Could never get this logic to work automatically like I wanted to. Have choose o only do this stuff manually and revist it at a later date
+    // const newLevels = calculateReputationLevels(
+    //   faction.notorietyLevel,
+    //   faction.prestigeLevel,
+    //   notorietyCount,
+    //   prestigeCount
+    // )
+
+    if (onChange) {
+      onChange({
+        ...faction,
+        prestigeCount,
+        notorietyCount,
+      })
+    }
+  }
+
+  const handleLevelChange = ({ level, key }: { level: number; key: 'notorietyLevel' | 'prestigeLevel' }) => {
+    if (onChange) {
+      onChange({
+        ...(faction as FactionReputation),
+        [key]: faction[key] === level ? 0 : level,
+      })
+    }
+  }
+
   return (
-    <div className="flex w-full flex-wrap" key={faction.id}>
+    <div className="flex w-full flex-wrap items-center" key={faction.id}>
       <div className="flex w-1/2 flex-wrap items-center pr-4">
         <div className="w-1/3 text-lg">
           {faction.name} <span className="pl-2 text-xl opacity-40">({faction.level})</span>
@@ -65,17 +157,31 @@ const ReputationManagerRow = ({ faction }: ReputationManagerRowProps) => {
             {notorietyLevels.map((level) => {
               return (
                 <div className="flex w-1/3 flex-wrap items-center justify-between px-6 text-lg" key={`level-${level}`}>
-                  <p>-{level}</p>
+                  <p
+                    className={cn('flex h-6 w-6 cursor-pointer items-center justify-center', {
+                      'bg-yellow-200': faction.notorietyLevel === level * -1,
+                    })}
+                    onClick={() => {
+                      handleLevelChange({
+                        level: level * -1,
+                        key: 'notorietyLevel',
+                      })
+                    }}
+                  >
+                    -{level}
+                  </p>
                   {notorietySteps.map((_, index) => {
                     const modifiedIndex = notorietySteps.length - index + (level - 1) * notorietyStepCount
                     const isChecked = modifiedIndex <= faction.notorietyCount
                     return (
                       <input
                         type="checkbox"
-                        name={String(modifiedIndex)}
+                        name="notorietyCount"
                         id={String(modifiedIndex)}
+                        value={modifiedIndex}
                         key={index}
-                        defaultChecked={isChecked}
+                        checked={isChecked}
+                        onChange={handleOnChange}
                       />
                     )
                   })}
@@ -96,14 +202,28 @@ const ReputationManagerRow = ({ faction }: ReputationManagerRowProps) => {
                   return (
                     <input
                       type="checkbox"
-                      name={String(modifiedIndex)}
+                      name="prestigeCount"
                       id={String(modifiedIndex)}
+                      value={modifiedIndex}
                       key={index}
-                      defaultChecked={isChecked}
+                      checked={isChecked}
+                      onChange={handleOnChange}
                     />
                   )
                 })}
-                <p>+{level}</p>
+                <p
+                  className={cn('flex h-6 w-6 cursor-pointer items-center justify-center rounded', {
+                    'bg-yellow-200': faction.prestigeLevel === level,
+                  })}
+                  onClick={() => {
+                    handleLevelChange({
+                      level: level,
+                      key: 'prestigeLevel',
+                    })
+                  }}
+                >
+                  +{level}
+                </p>
               </div>
             )
           })}
@@ -114,11 +234,22 @@ const ReputationManagerRow = ({ faction }: ReputationManagerRowProps) => {
 }
 
 export const ReputationManager = () => {
+  const [factions, setFactions] = useState(factionsState)
+
+  const handleReputationChange = (rep: FactionReputation) => {
+    setFactions((state) => {
+      const index = state.map((faction) => faction.id).indexOf(rep.id)
+      const newState = [...state]
+      newState[index] = rep
+      return newState
+    })
+  }
+
   return (
     <div className="flex w-full flex-wrap">
       {/* row for one faction */}
       {factions.map((faction) => {
-        return <ReputationManagerRow faction={faction} key={faction.id} />
+        return <ReputationManagerRow faction={faction} key={faction.id} onChange={handleReputationChange} />
       })}
 
       {/* bottom row for labels */}
